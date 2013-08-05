b22ea029472c27b039e1396daff306bce1c457a8
Lev Givon <lev@columbia.edu>
2013-08-05 09:33:33 -0400

    Add support for reloading pylab.

--- duster.py
+++ duster.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python                                                                                             
-"""                                                                                                               
-IPython extension to reset namespace and reload several modules automatically                                     
+#!/usr/bin/env python 
+"""
+IPython extension to reset namespace and reload several modules automatically
 """
 
 import importlib
 import IPython
 
-# Modules to reload; if no name is specified as the                                                               
+# Modules to reload; each element contains the module and the name to which
+# it should be imported (or '' if it should be imported as-is):
 modules = [('numpy', 'np'),
            ('scipy', 'sp')]
 
@@ -19,11 +20,21 @@ def duster(self, arg=''):
         else:
             ip.ns_table['user_global'][m[1]] = importlib.import_module(m[0])
 
+    # If IPython was started in pylab mode, reload symbols in pylab module directly
+    # into namespace:
+    cfg = ip.config
+    if (cfg.has_key('IPKernelApp') and cfg['IPKernelApp'].has_key('pylab')) or \
+       (cfg.has_key('TerminalIPythonApp') and cfg['TerminalIPythonApp'].has_key('pylab')):
+        ip.ns_table['user_global']['pylab'] = importlib.import_module('pylab')
+        for name in ip.ns_table['user_global']['pylab'].__dict__.keys():
+            ip.ns_table['user_global'][name] = \
+                ip.ns_table['user_global']['pylab'].__dict__[name]
+
 duster.__doc__ = \
-"""                                                                                                               
-Reset namespace and (re)load several modules automatically.                                                       
-Modules currently reloaded:                                                                                       
-                                                                                                                  
+"""
+Reset namespace and (re)load several modules automatically. 
+Modules currently reloaded:
+ 
 """
 for m in modules:
     if len(m) == 1:
