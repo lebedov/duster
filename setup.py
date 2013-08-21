21d20945d6691ffb124b44de8011337e51ecdccf
Lev Givon <lev@columbia.edu>
2013-08-21 09:33:43 -0400

    Move source, add setup.py.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+
+from setuptools import find_packages
+from setuptools import setup
+
+NAME =               'duster'
+VERSION =            '0.01'
+AUTHOR =             'Lev Givon'
+AUTHOR_EMAIL =       'lev@columbia.edu'
+URL =                'http://github.com/lebedov/duster/'
+DESCRIPTION =        'Reset IPython namespace'
+LONG_DESCRIPTION =   DESCRIPTION
+DOWNLOAD_URL =       URL
+LICENSE =            'BSD'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development']
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'): 
+        os.remove('MANIFEST')
+
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = AUTHOR,
+        author_email = AUTHOR_EMAIL,
+        license = LICENSE,
+        classifiers = CLASSIFIERS,
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        url = URL,
+        packages = find_packages()
+
+        install_requires = ['ipython>=0.13'],
+        extras_require = dict(
+            numpy = ['numpy']
+        ))
